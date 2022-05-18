6b63caa2b78974d3d25c45d2ead1e4c22b8097b2
Dani <dani@pop-os.localdomain>
2022-05-18 10:23:00 +0200

    Dropped json to html library, converted manually (works)

--- src/script.js
+++ src/script.js
@@ -28,57 +28,45 @@ const outFile = outFileName.join('.') + '.html';
 // reading and parsing JSON data
 const rawData = fs.readFileSync('./filtered-JSON/' + input[0]);
 const parsedData = JSON.parse(rawData);                 // type: object
+const stringData = JSON.stringify(parsedData);
 
-let tag;
-
-let template = {'<>':'div', 'class':'card', 'html':[
-                {'<>':'h2', 'text':'${info.name}'},
-                {'<>':'h3', 'class':function(){return this.info.severity},'text': '${info.severity}'},
-                /*{'<>':'p', 'text':'Tags:'},
-                {'<>':'ul', 'html': [
-                   {'<>':'li', 'text':function(){return this.info.tag}, 'obj':function(){return this.info.tags}},
-                ]},*/
-                {'<>':'p', 'text':'CVE:'},
-                {'<>':'ul', 'html': [
-                   {'<>':'li', 'obj':function(){ this.info.tags.forEach(element => {
-                       tag = element;
-                   });}}, 
-               ]},
-]};
+// directly writing the HTML file with file system library
+fs.appendFileSync('./out/' + outFile, '<link rel="stylesheet" href="../src/styles.css">', 'UTF-8', {'flags': 'a'});
+fs.appendFileSync('./out/' + outFile, '<h1> Found vulnerabilities for ' + outFileName.join('.') + '</h1>', 'UTF-8', {'flags': 'a'});
 
 
-// little test
-let html = json2html.render(parsedData, template);
-console.log(html); 
-fs.appendFileSync('./out/' + outFile, html, 'UTF-8', {'flags': 'a'});
-
-
-
-// library-less hardcode solution if nuclei JSON output issue is not fixed, not yet discarded
-// fs.appendFileSync('./out/' + outFile, '<h1> Found vulnerabilities for ' + outFileName.join('.') + '</h1>', 'UTF-8', {'flags': 'a'});
-// for each file, append HTML tag and conditionally set color
-
-/*
+// dropped the library since it wouldn't read json arrays with no key
 parsedData.forEach((element) => {
+    // putting every vulnerability inside a card
     fs.appendFileSync('./out/' + outFile, '<div class="card">', 'UTF-8', {'flags': 'a'});
+    
+    // header with element name
+    fs.appendFileSync('./out/' + outFile, '<h2>' + element.info.name + '</h2>', 'UTF-8', {'flags': 'a'});
     
     fs.appendFileSync('./out/' + outFile, '<h3 ' + 'class = ' + element.info.severity + '>' + element.info.severity + '</h3>', 'UTF-8', {'flags': 'a'});
 
-    fs.appendFileSync('./out/' + outFile, '<h2>' + element.info.name + '</h2>', 'UTF-8', {'flags': 'a'});
-
-    fs.appendFileSync('./out/' + outFile, '<p> TAGS: ' + element.info.tags, 'UTF-8', {'flags': 'a'});
-
-    fs.appendFileSync('./out/' + outFile, '<p> CLASSIFICATION: ', 'UTF-8', {'flags': 'a'});
+    // tag list
+    fs.appendFileSync('./out/' + outFile, '<p> TAGS: ', 'UTF-8', {'flags': 'a'});
     element.info.tags.forEach((tag) => {
         fs.appendFileSync('./out/' + outFile, '<li>' + tag + '</li>', 'UTF-8', {'flags': 'a'});
     });
+    fs.appendFileSync('./out/' + outFile, '</p><br>', 'UTF-8', {'flags': 'a'});
+
+    // https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-6210
+    fs.appendFileSync('./out/' + outFile, '<p> CVE CLASSIFICATION: ', 'UTF-8', {'flags': 'a'});
+    element.info.classification.cve.forEach((element) => {
+        fs.appendFileSync('./out/' + outFile,           // link for more information
+        '<li> <a href = https://cve.mitre.org/cgi-bin/cvename.cgi?name=' + element.toUpperCase() + '>' + element.toUpperCase() + '</a></li>', 'UTF-8', {'flags': 'a'});
+    });
+    fs.appendFileSync('./out/' + outFile, '</p>', 'UTF-8', {'flags': 'a'});
 
     if(element.info.description)
         fs.appendFileSync('./out/' + outFile, '<p>' + element.info.description + '</p>', 'UTF-8', {'flags': 'a'});
-    
+
+
     fs.appendFileSync('./out/' + outFile, '</div>', 'UTF-8', {'flags': 'a'});
 });
-*/
+
 
 console.log(input);
 console.log(typeof parsedData);
